c90f46ad6e2ab9328888fdd0ee9a67590187356a
alexborda <alexander.borda@gmail.com>
2025-02-09 16:23:37 -0400

    Fix index.html issue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  root: ".",  // ✅ Asegura que la raíz es el directorio actual
+  build: {
+    outDir: "dist",
+  },
 })
